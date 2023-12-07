fa4dc70ff9d2b45c366541e23f0252c496c2b806
srishti <isinghsrishti@gmail.com>
2023-12-07 17:10:39 +0530

    fix: sanity config

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,7 +5,7 @@ import { schemaTypes } from "./schemas";
 export default defineConfig({
   name: process.env.SANITY_NAME || "sanity-nextjs-site",
   title: process.env.SANITY_TITLE || "Sanity Next.js Site",
-  projectId: process.env.SANITY_PROJECT_ID || "ga8lllhf",
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID || "m1bph78k",
   dataset: process.env.SANITY_DATASET || "production",
   basePath: process.env.SANITY_BASE_PATH || "/studio",
   plugins: [deskTool()],
