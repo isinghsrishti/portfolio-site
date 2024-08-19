0b93405312724bf0214f85603671d764fba87ee7
srishti <isinghsrishti@gmail.com>
2024-08-19 06:48:23 +0530

    update work experience

--- app/(site)/components/Job.tsx
+++ app/(site)/components/Job.tsx
@@ -6,7 +6,7 @@ export default async function Job() {
   const job: JobType[] = await getJob();
 
   return (
-    <section className="mt-32">
+    <section className="mt-4">
       <div className="mb-16">
         <h2 className="font-semibold text-4xl mb-4">Work Experience</h2>
       </div>
